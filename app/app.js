344c2478099944935d7d976c40a419fb9a468dc7
danyu <danyuyou@gmail.com>
2018-01-01 10:06:59 -0800

    Adjusted layout.

--- app/app.js
+++ app/app.js
@@ -2,15 +2,28 @@ angular
 .module("dyApp",[])
 .controller("dyAppCtrl",function($scope){
 
-    $scope.tpls=[
-            {name:'droid',url:"svg-tpls/droid.tpl.html"},
-            {name:'places',url:"svg-tpls/places.tpl.html"}
+    $scope.navs=[
+            {name:'places',url:"tpls/places.tpl.html",visible:true},
+            {name:'me',url:"tpls/droid.tpl.html",visible:false},
+            {name:'experiences', url:"tpls/experiences.tpl.html",visible:false}
+            
         ];
-    
+   
+
+    $scope.placesJSON = JSON.stringify($scope.places);
+
+    $scope.experiences =[
+        {year:{start:"2013",end:"2015"}, title:"Software Engineer"},
+        {year:{start:"2015",end:"now" }, title:"Programmer Analyst"}
+    ];
+
     $scope.curTpl="";
 
     $scope.updateContent = function(tplName){
-        $scope.curTpl=(_.find($scope.tpls, {name:tplName})).url;
+        // $scope.curTpl=(_.find($scope.navs, {name:tplName})).url;
+        _.each($scope.navs, function(nav){
+            nav.visible=nav.name==tplName;
+        });
     };
 
 });
